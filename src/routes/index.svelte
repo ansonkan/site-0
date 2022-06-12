210c25c056547a6420e72ea2698780fdc2721779
Anson Kan <ansonkan@tutanota.com>
2022-06-12 18:11:01 +0800

    add browser check

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { Scene, OrthographicCamera, WebGLRenderer } from 'three';
+  import { browser } from '$app/env';
+  import { onMount, onDestroy } from 'svelte';
+  import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
   import { Text } from 'troika-three-text';
 
   let width = 0;
@@ -8,19 +9,31 @@
 
   let canvas: HTMLCanvasElement;
   let scene: Scene;
-  let camera: OrthographicCamera;
+  let camera: PerspectiveCamera;
   let renderer: WebGLRenderer;
 
   onMount(() => {
-    init();
+    if (browser) {
+      init();
+    }
+  });
+
+  onDestroy(() => {
+    if (browser) {
+      window.removeEventListener('resize', onWindowResize);
+    }
   });
 
   function init() {
     width = window.innerWidth;
     height = window.innerHeight;
 
-    camera = new OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 1000);
-    camera.position.set(0, 0, 100);
+    renderer = new WebGLRenderer({ antialias: true, canvas });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.set(5, -2, 10);
 
     scene = new Scene();
     scene.add(camera);
@@ -30,16 +43,40 @@
 
     // Set properties to configure:
     text.text = 'Hello world!';
-    text.fontSize = 0.2;
-    text.position.z = -2;
+    text.fontSize = 2;
     text.color = 0x9966ff;
 
     // Update the rendering:
     text.sync();
+
+    window.addEventListener('resize', onWindowResize);
+
+    // render();
+    animate();
+  }
+
+  function onWindowResize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+
+    // render();
+  }
+
+  function render() {
+    renderer.render(scene, camera);
+  }
+
+  function animate() {
+    window.requestAnimationFrame(animate);
+
+    render();
   }
 </script>
-
-/// <reference paths="../../types/troika-three-text/index.d.ts" />
 
 <canvas bind:this={canvas} />
 
