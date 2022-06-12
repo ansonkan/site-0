11fb7c1bdc144f23e41631e29213f618745e1f90
Anson Kan <ansonkan@tutanota.com>
2022-06-12 18:50:56 +0800

    fix troika-three-text module types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference types="three" />
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
@@ -9,4 +10,43 @@ declare namespace App {
   // interface Stuff {}
 }
 
-declare module 'troika-three-text';
+declare module 'troika-three-text' {
+  export class Text extends THREE.Object3D {
+    text: string;
+    anchorX: number | string;
+    anchorY: number | string;
+    curveRadius: number;
+    direction: 'auto' | 'ltr' | 'rtl';
+    font: string | null;
+    fontSize: number;
+    letterSpacing: number;
+    lineHeight: number | string;
+    maxWidth: number;
+    overflowWrap: 'normal' | 'break-word';
+    textAlign: 'left' | 'right' | 'center' | 'justify';
+    textIndent: number;
+    whiteSpace: 'normal' | 'nowrap';
+    material: THREE.Material | null;
+    color: string | number | THREE.Color | null;
+    outlineWidth: number | string;
+    outlineColor: string | number | THREE.Color;
+    outlineOpacity: number;
+    outlineBlur: number | string;
+    outlineOffsetX: number | string;
+    outlineOffsetY: number | string;
+    strokeWidth: number | string;
+    strokeColor: string | number | THREE.Color;
+    strokeOpacity: number;
+    fillOpacity: number;
+    depthOffset: number;
+    clipRect: [number, number, number, number] | null;
+    orientation: string;
+    glyphGeometryDetail: number;
+    sdfGlyphSize: number | null;
+    gpuAccelerateSDF: boolean;
+    debugSDF: boolean;
+
+    sync: (callback?: (...args: unknown) => unknown) => void;
+    dispose: () => void;
+  }
+}
