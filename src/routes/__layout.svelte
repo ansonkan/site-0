227138bac58f57a62db02cc9c9979c435f8f6edd
Anson Kan <ansonkan@tutanota.com>
2022-08-09 07:34:45 +0800

    change button focus style

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,5 +2,16 @@
   <slot />
 </main>
 
-<style lang="scss" windi:preflights:global windi:safelist:global>
+<style lang="scss" windi:preflights:global windi:safelist:global global>
+  a,
+  input,
+  textarea,
+  button {
+    transition: box-shadow 0.3s ease, color 0.3s ease, font-size 0.3s ease;
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 3px rgba(21, 156, 228, 0.4);
+    }
+  }
 </style>
